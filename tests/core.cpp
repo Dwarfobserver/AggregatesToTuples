5a0db5cec9cc7335cd6bd89ee6228dc422238552
Sidney Congard <sidney.congard@gmail.com>
2018-03-31 22:37:24 -0400

    added test which move struct in tuple

--- tests/core.cpp
+++ tests/core.cpp
@@ -1,10 +1,12 @@
 
 #include "catch.hpp"
 #include <core.hpp>
-
+#include <memory>
+#include <string>
 
 namespace {
     struct person {
+        std::string name;
         float const size;
         int age;
     };
@@ -13,29 +15,47 @@ namespace {
     public:
         wrapper(int v) : value(v) {}
     };
+    struct movable {
+        std::unique_ptr<int> ptr;
+    };
 }
 
 TEST_CASE("constexpr traits") {
-    CHECK( att::is_aggregate<person>);
     CHECK(!att::is_aggregate<wrapper>);
+    CHECK( att::is_aggregate<person>);
+    CHECK( att::is_aggregate<movable>);
 
-    CHECK( att::arity_of<person> == 2);
+    CHECK(att::arity_of<person>  == 3);
+    CHECK(att::arity_of<movable> == 1);
 }
 
 TEST_CASE("core functions and types") {
-    person p{ 1.75f, 24 };
+    person p{ "Brown", 1.75f, 24 };
+
     auto tuple = att::to_tuple(p);
     auto tuple_refs = att::as_tuple(p);
 
     p.age += 2;
-    CHECK(std::get<1>(tuple) == 24);
-    CHECK(std::get<1>(tuple_refs) == 26);
+    CHECK(std::get<2>(tuple) == 24);
+    CHECK(std::get<2>(tuple_refs) == 26);
 
     att::to_tuple_t<person> tuple2 = tuple_refs;
-    std::get<1>(tuple2) += 1;
-    CHECK(p.age == 26);
+    std::get<0>(tuple2) += " !";
+    CHECK(p.name == "Brown");
 
     att::as_tuple_t<person> tuple_refs2 = tuple_refs;
-    std::get<1>(tuple_refs2) += 1;
-    CHECK(p.age == 27);
+    std::get<0>(tuple_refs2) += " !";
+    CHECK(p.name == "Brown !");
+}
+
+TEST_CASE("make tuple by move") {
+    movable m{ std::make_unique<int>(42) };
+
+    auto tuple_refs = att::as_tuple(m);
+    auto tuple = att::to_tuple(std::move(m));
+
+    CHECK(m.ptr.get() == nullptr);
+
+    auto& ptr = std::get<0>(tuple);
+    CHECK(*ptr == 42);
 }
