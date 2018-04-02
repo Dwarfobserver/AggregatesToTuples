e9dc7040132af85bbe12680c2a117dd1cdd9ec82
Sidney Congard <sidney.congard@gmail.com>
2018-04-01 20:27:13 -0400

    added custom serialization test

--- tests/serialization.cpp
+++ tests/serialization.cpp
@@ -38,3 +38,43 @@ TEST_CASE("ostream serialization") {
     CHECK(unknown.i.age  == 68);
     CHECK(unknown.i.size == 182);
 }
+
+namespace {
+    struct number_serializer {
+        int* buffer;
+
+        number_serializer& operator<<(int i) {
+            *(buffer++) = i;
+            return *this;
+        }
+        number_serializer& operator>>(int& i) {
+            i = *(buffer++);
+            return *this;
+        }
+    };
+    struct info_pack {
+        info i1;
+        info i2;
+        info i3;
+    };
+}
+
+
+TEST_CASE("custom serialization") {
+    int buffer[10];
+
+    info_pack const infos = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+    number_serializer stream { buffer };
+    using namespace att::operators;
+
+    stream << infos;
+
+    info_pack infos_copy = {};
+
+    stream.buffer = buffer;
+    stream >> infos_copy;
+
+    CHECK(infos.i1.size == infos_copy.i1.size);
+    CHECK(infos.i3.age  == infos_copy.i3.age);
+}
